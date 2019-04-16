e5283c5ae388c521ddfcaad6fbc888e2d7cb19f0
BarathArivazhagan <barathmacdec91@gmail.com>
2019-04-16 09:32:09 +0530

    spring boot version update to 2.1.4.RELEASE

--- zipkin-client-2/src/main/java/com/barath/app/Client1.java
+++ zipkin-client-2/src/main/java/com/barath/app/Client1.java
@@ -1,6 +1,7 @@
 package com.barath.app;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name="client",url="${microservice.client.url}")
